4e2144d6ab5b5dce2263d21ab05b1d853ee44a33
greatroot <icujoa@gmail.com>
2021-07-30 20:56:55 +0900

    3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -5,18 +5,25 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  //console.log("I'm in the middle!");
+  console.log(`Someone is going to: ${req.url}`);
+  //return res.send("I have the power now!");
+  next();
+};
+
 const handleHome = (req, res) => {
   //   console.log("Somebody is trying to go home.");
-  //return res.end();
-  return res.send("I still love you.");
+  return res.end();
+  //return res.send("<h1>I still love you.</h1>");
 };
 const handleLogin = (req, res) => {
-  return res.send("Login here.");
+  return res.send({ message: "Login here." });
 };
 
 //console.log("I'm good!!!!!!");
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
