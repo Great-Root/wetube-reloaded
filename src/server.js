0539abf90feb29402c0b5d8e6a7fec4e781e5d95
greatroot <icujoa@gmail.com>
2021-07-30 20:31:43 +0900

    3.3 Responses

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 //const express = require("express"); 예전문법
 import express from "express";
+
 const PORT = 4000;
+
 const app = express();
 
+const handleHome = (req, res) => {
+  //   console.log("Somebody is trying to go home.");
+  //return res.end();
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+
 //console.log("I'm good!!!!!!");
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listenting on port http://localhost:${PORT} 🚀`);
