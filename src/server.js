4d2a1ee097a80c54b261163e5941361fca78c609
greatroot <icujoa@gmail.com>
2021-08-01 13:03:35 +0900

    3.6 Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -5,26 +5,40 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
+const logger = (req, res, next) => {
   //console.log("I'm in the middle!");
-  console.log(`Someone is going to: ${req.url}`);
+  console.log(`${req.method} ${req.url}`);
   //return res.send("I have the power now!");
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
   next();
 };
 
 const handleHome = (req, res) => {
   //   console.log("Somebody is trying to go home.");
-  return res.end();
-  //return res.send("<h1>I still love you.</h1>");
+  //return res.end();
+  return res.send("I love middlewares");
 };
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
+// const handleLogin = (req, res) => {
+//   return res.send({ message: "Login here." });
+// };
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
 };
 
 //console.log("I'm good!!!!!!");
+app.use(logger);
+app.use(privateMiddleware);
 
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin);
+app.get("/", handleHome);
+//app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listenting on port http://localhost:${PORT} 🚀`);
